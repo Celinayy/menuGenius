9d8c30330b1f5f4bb68ab438a8cfb6d46c4ae0e6
Szilard750913 <letenyei.szilard@students.jedlik.eu>
2024-01-08 06:10:32 +0100

    chore: edit-database-files

--- Backend/database/migrations/2024_01_07_202644_create_products_table.php
+++ Backend/database/migrations/2024_01_07_202644_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id('id');
             $table->string('name')->unique();
-            $table->string('descripton');
+            $table->string('description');
             $table->unsignedBigInteger('category_id');
             $table->string('packing');
             $table->integer('price');
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource("product", ProductController::class)->except(["edit", "create"]);
+
+Route::resource("ingredient", ProductController::class)->except(["edit", "create"]);
