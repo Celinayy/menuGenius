d22f7164324d45c9cd581095042a3399518b23f1
Szilard750913 <letenyei.szilard@students.jedlik.eu>
2024-01-08 23:39:44 +0100

    feat: create classes - purchase, reservation, user

---
+++ MG_Admin_GUI/ViewModels/PurchaseViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MG_Admin_GUI.Models;
+
+namespace MG_Admin_GUI.ViewModels
+{
+    public class PurchaseViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Purchase> _Purchases;
+        public ObservableCollection<Purchase> Purchases
+        {
+            get { return _Purchases; }
+            set
+            {
+                if (_Purchases != value)
+                {
+                    _Purchases = new ObservableCollection<Purchase>(value.OrderBy(purchase => purchase.date));
+                    OnPropertyChanged(nameof(Purchases));
+                }
+            }
+        }
+
+
+
+        public PurchaseViewModel()
+        {
+            Purchases = Purchase.GetPurchases();
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
---
+++ MG_Admin_GUI/ViewModels/UserViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MG_Admin_GUI.Models;
+
+namespace MG_Admin_GUI.ViewModels
+{
+    public class UserViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<User> _Users;
+        public ObservableCollection<User> Users
+        {
+            get { return _Users; }
+            set
+            {
+                if (_Users != value)
+                {
+                    _Users = value;
+                    OnPropertyChanged(nameof(Users));
+                }
+            }
+        }
+
+        private User _selectedUser;
+        public User selectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                if (value != _selectedUser)
+                {
+                    _selectedUser = value;
+                    OnPropertyChanged(nameof(selectedUser));
+                }
+            }
+        }
+
+        public UserViewModel()
+        {
+            Users = User.GetUsers();
+            selectedUser = null;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
