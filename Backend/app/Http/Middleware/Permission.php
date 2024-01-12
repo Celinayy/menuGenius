a71d079afd946ee87501319c18596bfd89377b48
Szilard750913 <letenyei.szilard@students.jedlik.eu>
2024-01-12 12:06:43 +0100

    chore: update permission.php

--- Backend/app/Http/Middleware/Permission.php
+++ Backend/app/Http/Middleware/Permission.php
@@ -13,12 +13,12 @@ class Permission
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $role): Response
     {
         if ($request->user() && $request->user()->admin == true)
         {
             return $next($request);
         }
-        return response()->json(['error' => 'Unauthorized'], 403);
+        return response()->json(['error' => 'Nincs jogosultságod!'], 403);
     }
 }
