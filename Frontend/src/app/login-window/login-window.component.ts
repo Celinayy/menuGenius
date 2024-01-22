49d9a738e9dbc1430a770a2c435031379d37b075
hawaroaladar1 <vangelcelina9@gmail.com>
2024-01-22 12:21:29 +0100

    feat: show toast on login window

--- Frontend/src/app/login-window/login-window.component.ts
+++ Frontend/src/app/login-window/login-window.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { AuthService } from '../services/auth.service';
+import { catchError, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login-window',
@@ -11,19 +14,24 @@ export class LoginWindowComponent {
   public email: string = "";
   public password: string = "";
 
-  public showSuccessfulLoginToast: boolean = false;
-  public showSuccessfulRegisterToast: boolean = false;
-
-  constructor(public menu: MenuComponent, private authService: AuthService) {}
+  constructor(public menu: MenuComponent, private authService: AuthService, private toast: ToastrService) { }
 
   public login() {
-    this.authService.login(this.email, this.password).subscribe((result) => {
-      localStorage.setItem("token", result.token);
-      this.showSuccessfulLoginToast = true;
-      setTimeout(() => {
-        this.showSuccessfulLoginToast = false;
-      }, 2000);
-    });
+    this.authService.login(this.email, this.password)
+      .pipe(catchError((err: HttpErrorResponse) => {
+        for (const message of err.error.email ?? []) {
+          this.toast.error(message)
+        }
+        for (const message of err.error.password ?? []) {
+          this.toast.error(message)
+        }
+        return throwError(() => err)
+      }))
+      .subscribe((result) => {
+        localStorage.setItem("token", result.token);
+        this.toast.success("Sikeres bejelentkezés!")
+      });
+
   }
 
 }
