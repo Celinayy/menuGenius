26f69f462a5f7008b81420b96ce4ff7135162b9b
hawaroaladar1 <vangelcelina9@gmail.com>
2024-02-01 16:50:03 +0100

    feat: Show toast when user click on the star

--- Frontend/src/app/services/favourite.service.ts
+++ Frontend/src/app/services/favourite.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ProductModel } from '../models/product-model';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -8,18 +9,24 @@ import { ProductModel } from '../models/product-model';
 export class FavouriteService {
 
   private favProducIds: number[] = [];
-  private url = "http://localhost:8000/api/product"
+  private url = "http://localhost:8000/api"
 
-
-
-  constructor(private connection: HttpClient) { }
+  constructor(private connection: HttpClient, private toast: ToastrService) { }
 
   private loadFavourites() {
-    return this.connection.get<ProductModel[]>(`${this.url}`)
+    return this.connection.get<ProductModel[]>(`${this.url}/products/`)
   }
 
   public addToFavourite(product: ProductModel) {
-    return this.favProducIds.push(product.id)
+    this.favProducIds.push(product.id)
+
+    this.connection.post<{ message: string }>(`${this.url}/product/${product.id}/addToFavorites`, {}, {
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem("token")}`,
+      },
+    }).subscribe((result) => {
+      this.toast.success(result.message);
+    });
   }
 
   public removeFromFavourite(product: ProductModel) {
@@ -29,7 +36,15 @@ export class FavouriteService {
         result.push(favProducId)
       }
     }
-    this.favProducIds = result
+    this.favProducIds = result;
+
+    return this.connection.post<{ message: string }>(`${this.url}/product/${product.id}/removeFromFavorites`, {}, {
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem("token")}`,
+      },
+    }).subscribe((result) => {
+      this.toast.success(result.message);
+    });
   }
 
   public isFavourite(product: ProductModel) {
