228d6f1c9ff4f8732e8db63f2c005e611e68eaf4
Vangel Celina Hanna <vangelcelina9@gmail.com>
2024-03-04 09:09:01 +0100

    test: login

--- Frontend/tests/auth.spec.ts
+++ Frontend/tests/auth.spec.ts
@@ -1,20 +1,50 @@
-import { test } from "@playwright/test";
+import { Page, test } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
-test("Regisztráció", async ({ page }) => {
+const register = async (page: Page, fullName: string, email: string, phone: string, password: string) => {
     await page.goto("/");
 
     await page.getByTestId("open-auth-dialog-button").click();
 
     await page.getByTestId("open-register-window-button").click();
 
-    const password = faker.internet.password();
-
-    await page.getByTestId("full-name-input").fill(faker.person.fullName());
-    await page.getByTestId("email-input").fill(faker.internet.email());
-    await page.getByTestId("phone-input").fill(faker.phone.number());
+    await page.getByTestId("full-name-input").fill(fullName);
+    await page.getByTestId("email-input").fill(email);
+    await page.getByTestId("phone-input").fill(phone);
     await page.getByTestId("password-input").fill(password);
     await page.getByTestId("password-again-input").fill(password);
     await page.getByTestId("register-button").click();
+};
+
+const login = async (page: Page, email: string, password: string) => {
+    await page.goto("/");
+
+    await page.getByTestId("open-auth-dialog-button").click();
+
+    await page.getByTestId("open-login-window-button").click();
+
+    await page.getByTestId("email-input").fill(email);
+    await page.getByTestId("password-input").fill(password);
+    await page.getByTestId("login-button").click();
+};
+
+test("Regisztráció", async ({ page }) => {
+    await page.goto("/");
+
+    const fullName = faker.person.fullName();
+    const email = faker.internet.email();
+    const phone = faker.phone.number();
+    const password = faker.internet.password();
+
+    await register(page, fullName, email, phone, password);
 });
 
+test("Bejelentkezés", async ({ page }) => {
+    const fullName = faker.person.fullName();
+    const email = faker.internet.email();
+    const phone = faker.phone.number();
+    const password = faker.internet.password();
+
+    await register(page, fullName, email, phone, password);
+    await login(page, email, password);
+});
