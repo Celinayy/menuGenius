b716d23ae3c3ccf9a24b81264c9b0a3dd0b58a30
Szilard750913 <letenyei.szilard@students.jedlik.eu>
2024-03-11 01:07:38 +0100

    chore: upload project files

---
+++ MG_Admin_GUI_v2.2/Models/password_reset_token.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MG_Admin_GUI.Models;
+
+public partial class password_reset_token
+{
+    public string email { get; set; } = null!;
+
+    public string token { get; set; } = null!;
+
+    public DateTime? created_at { get; set; }
+}
